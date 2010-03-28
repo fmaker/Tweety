89a927c5cbf70d7223d62f9e5245c06468e82bf8
Frank Maker <fmaker@handycodeworks.com>
2010-03-27 23:15:28 -0700

    +Stubs for TweetyTest

---
+++ tests/src/com/handycodeworks/tweety/tests/TweetyTest.java
@@ -0,0 +1,45 @@
+package com.handycodeworks.tweety.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.handycodeworks.tweety.Tweety;
+
+public class TweetyTest extends ActivityInstrumentationTestCase2<Tweety> {
+
+	public TweetyTest(String name) {
+		super(name, null);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public final void testOnStop() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testOnCreateBundle() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testOnClick() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testOnCreateOptionsMenuMenu() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testOnOptionsItemSelectedMenuItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testOnKey() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
